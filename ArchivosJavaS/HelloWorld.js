3ac530cfc2a8e4fbf169849578d8e130df86c3f2
Alejandro <requenalosenlaces@gmail.com>
2022-10-05 13:44:14 +0200

    Subida Carpeta Curso

---
+++ ArchivosJavaS/HelloWorld.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta http-equiv="Content-Type"
+ content="text/html;charset=utf-8">
+ <title>Hola Mundo</title>
+<script>
+ alert("Prueba de JavaScript");
+ </script>
+ </head>
+ <body>
+ <h1>Ejemplo 1: código embebido</h1>
+</body>
+<html>
